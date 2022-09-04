8877b3bf344a0ad5e4e76e3b381a36a2332f1977
gislainm <gmuhikira2@gmail.com>
2022-09-04 16:24:53 -0500

    prepping to merge

--- server/model/responseInfo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*eslint-disable */
-module.exports = class Response {
-    constructor(error, message, data) {
-        this.error = error;
-        this.message = message;
-        this.data = data;
-    }
-}
